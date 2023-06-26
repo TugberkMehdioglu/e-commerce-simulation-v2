6abeee3b2ff0bd0c7ff2facccac72b4abd9af71c
Tuğberk <tugberkmehdioglu@gmail.com>
2023-06-26 19:52:11 +0300

    Primary key configured

--- Project.ENTITIES/Interfaces/IEntity.cs
+++ Project.ENTITIES/Interfaces/IEntity.cs
@@ -9,7 +9,6 @@ namespace Project.ENTITIES.Interfaces
 {
     public interface IEntity
     {
-        public virtual string Id { get { return Id; } set { Id = value; } }
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
