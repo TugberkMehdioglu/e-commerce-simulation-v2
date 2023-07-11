81f9fe6fec9d5063ef354e846660299ee2246c78
Tuğberk <tugberkmehdioglu@gmail.com>
2023-07-11 22:31:45 +0300

    Login proccess added

--- Project.MVCUI/Controllers/HomeController.cs
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Project.BLL.ManagerServices.Abstracts;
+using Project.BLL.ManagerServices.Concretes;
+using Project.COMMON.Extensions;
 using Project.ENTITIES.Models;
+using Project.MVCUI.Extensions;
+using Project.MVCUI.ViewModels;
 using System.Diagnostics;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Project.MVCUI.Controllers
 {
@@ -9,10 +15,14 @@ namespace Project.MVCUI.Controllers
     public class HomeController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IAppUserProfileManager _appUserProfileManager;
 
-        public HomeController(SignInManager<AppUser> signInManager)
+        public HomeController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, IAppUserProfileManager appUserProfileManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
+            _appUserProfileManager = appUserProfileManager;
         }
 
         [Route("/")]
@@ -22,6 +32,55 @@ namespace Project.MVCUI.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel request, string? returnUrl = null)
+        {
+            if (!ModelState.IsValid) return View(request);
+
+            returnUrl ??= "/Home";
+
+            AppUser? appUser = await _userManager.FindByEmailAsync(request.Email);
+            if(appUser == null)
+            {
+                ModelState.AddModelErrorWithOutKey("Email veya şifre hatalı");
+                return View();
+            }
+
+            SignInResult result = await _signInManager.PasswordSignInAsync(appUser, request.PasswordHash, request.RememberMe, true);
+            if (result.IsLockedOut)
+            {
+                ModelState.AddModelErrorWithOutKey("30 dakika boyunca giriş yapamazsınız");
+                return View();
+            }
+            else if (!result.Succeeded)
+            {
+                ModelState.AddModelErrorWithOutKey("Email veya şifre hatalı", $"Başarısız giriş sayısı => {await _userManager.GetAccessFailedCountAsync(appUser)}");
+                return View();
+            }
+            
+            if(!await _userManager.IsEmailConfirmedAsync(appUser))
+            {
+                ModelState.AddModelErrorWithOutKey("Lütfen hesabınızı aktif hale getiriniz, mail'lerinizi kontrol ediniz");
+                await _signInManager.SignOutAsync();//So that the cookie still does not remain in the browser
+                return View();
+            }
+
+            AppUserProfile? appUserProfile = _appUserProfileManager.Where(x => x.Id == appUser.Id).Select(x => new AppUserProfile() { Id = x.Id, ImagePath = x.ImagePath }).FirstOrDefault();
+            if (appUserProfile != null)
+            {
+                SessionViewModel session = new() { ImagePath = appUserProfile.ImagePath };
+                HttpContext.Session.SetSession("sessionVM", session);
+            }
+
+            return Redirect(returnUrl);
+        }
+
         public async Task LogOut()
         {
             await _signInManager.SignOutAsync();
@@ -29,11 +88,6 @@ namespace Project.MVCUI.Controllers
         }
 
         public IActionResult Terms()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
---
+++ Project.MVCUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Project.MVCUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        [EmailAddress(ErrorMessage = "{0} formatı yanlıştır")]
+        [MaxLength(80, ErrorMessage = "{0} alanı en fazla {1} karakter olabilir")]
+        [DataType(DataType.Text)]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        [MinLength(6, ErrorMessage = "Şifre en az {1} karakter olabilir")]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; } = null!;
+
+        [Display(Name = "Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
