2eedc253c3f5df69526db9b8048c72c521891fdb
Tuğberk <tugberkmehdioglu@gmail.com>
2023-07-11 23:57:46 +0300

    ForgetPassword proccess added

--- Project.MVCUI/Controllers/HomeController.cs
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Project.BLL.ManagerServices.Abstracts;
 using Project.BLL.ManagerServices.Concretes;
 using Project.COMMON.Extensions;
+using Project.COMMON.Tools;
 using Project.ENTITIES.Models;
 using Project.MVCUI.Extensions;
 using Project.MVCUI.ViewModels;
 using System.Diagnostics;
+using System.Web;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Project.MVCUI.Controllers
@@ -79,6 +81,75 @@ namespace Project.MVCUI.Controllers
             }
 
             return Redirect(returnUrl);
+        }
+
+        public IActionResult ForgetPassword()
+        {
+            if (TempData["resetPasswordAlert"] != null) ModelState.AddModelErrorWithOutKey(TempData["resetPasswordAlert"]!.ToString()!);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel request)
+        {
+            ModelState.Remove("NewPassword");
+            ModelState.Remove("NewPasswordConfirm");
+            if (!ModelState.IsValid) return View(request);
+
+            AppUser? appUser = await _userManager.FindByEmailAsync(request.Email);
+            if (appUser == null)
+            {
+                ModelState.AddModelErrorWithOutKey("Bu email adresine sahip kullanıcı bulunamadı");
+                return View(request);
+            }
+
+            string passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(appUser!);
+            string encodedToken = HttpUtility.UrlEncode(passwordResetToken);
+            string passwordResetLink = Url.Action("ResetToken", "Home", new { userId = appUser!.Id, token = encodedToken }, HttpContext.Request.Scheme)!;
+            string emailBody= $@"
+  <div style='max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 10px; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+    <div style='text-align: center;'>
+        <img src='https://cdn-icons-png.flaticon.com/512/6357/6357059.png' alt='Logo' style='width: 15%; height: auto;'>
+    </div>
+    <div style='margin-top: 30px;'>
+        <h2 style='text-align: center; color: rgb(46, 74, 150); font-family: sans-serif;'>Şifre değiştirme talebini aldık!</h2>
+        <p style='text-align: center;'><a href='{passwordResetLink}' style='color:#fff; display: inline-block;padding: 10px 20px;background-color: rgb(46, 74, 150);text-decoration: none;border-radius: 5px; font-family: sans-serif;'>Şifremi Değiştir</a></p>
+
+        <div style='text-align: center; font-family: sans-serif;'>
+          <p>Eğer bu talebi siz gerçekleştirmediyseniz herhangi bir işlem yapmanıza gerek yoktur.</p>
+        </div>
+    </div>
+</div>
+            ";
+
+            MailService.SendMailAsync(appUser.Email, emailBody, "Şifre Sıfırlama | TechByGamers");
+            TempData["success"] = "Şifre sıfırlama linki email adresinize gönderilmiştir";
+            return View();
+        }
+
+        [HttpGet("{userId}/{token}")]
+        public IActionResult ResetToken(string userId, string token)
+        {
+            if(userId == null || token == null)
+            {
+                TempData["resetPasswordAlert"] = "Geçersiz kullanıcı yada zaman aşımı gerçekleşti";
+                return RedirectToAction(nameof(ForgetPassword));
+            }
+
+            string decodedToken = HttpUtility.UrlDecode(token);
+            TempData["userId"] = userId;
+            TempData["token"] = decodedToken;
+
+            IEnumerable<IdentityError>? errors = HttpContext.Session.GetSession<IEnumerable<IdentityError>>("resetPasswordAlers");
+            if(errors != null)
+            {
+                ModelState.AddModelErrorListWithOutKey(errors);
+                HttpContext.Session.Remove("resetPasswordAlers");
+            }
+
+            return View();
         }
 
         public async Task LogOut()
---
+++ Project.MVCUI/ViewModels/ForgetPasswordViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Project.MVCUI.ViewModels
+{
+    public class ForgetPasswordViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "{0} alanı boş bırakılamaz")]
+        [EmailAddress(ErrorMessage = "{0} formatı yanlıştır")]
+        [MaxLength(80, ErrorMessage = "{0} alanı en fazla {1} karakter olabilir")]
+        [DataType(DataType.Text)]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        [StringLength(40, MinimumLength = 6, ErrorMessage = "{0} {2} ile {1} karakter arası olmalıdır")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; } = null!;
+
+        [Display(Name = "Şifre Tekrar")]
+        [Required(ErrorMessage = "{0} boş bırakılamaz")]
+        [StringLength(40, MinimumLength = 6, ErrorMessage = "{0} {2} ile {1} karakter arası olmalıdır")]
+        [Compare(nameof(NewPassword), ErrorMessage = "Girmiş olduğunuz şifreler uyuşmuyor")]
+        [DataType(DataType.Password)]
+        public string NewPasswordConfirm { get; set; } = null!;
+    }
+}
