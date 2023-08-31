583717282c1d008b338e663179d5bf0ad676db8c
Tuğberk <tugberkmehdioglu@gmail.com>
2023-08-31 16:34:43 +0300

    ConfirmOrder process completed

--- Project.MVCUI/Controllers/ShoppingController.cs
+++ Project.MVCUI/Controllers/ShoppingController.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Project.BLL.ManagerServices.Abstracts;
 using Project.COMMON.Extensions;
+using Project.COMMON.Tools;
 using Project.ENTITIES.Enums;
 using Project.ENTITIES.Models;
 using Project.MVCUI.Areas.Admin.AdminViewModels;
 using Project.MVCUI.Models.ShoppingTools;
+using Project.MVCUI.ViewModels;
 using Project.MVCUI.ViewModels.WrapperClasses;
 
 namespace Project.MVCUI.Controllers
@@ -16,13 +19,21 @@ namespace Project.MVCUI.Controllers
     {
         private readonly IProductManager _productManager;
         private readonly ICategoryManager _categoryManager;
+        private readonly IAppUserManager _appUserManager;
+        private readonly IOrderManager _orderManager;
+        private readonly IOrderDetailManager _orderDetailManager;
+        private readonly IAddressManager _addressManager;
         private readonly IMapper _mapper;
 
-        public ShoppingController(IProductManager productManager, ICategoryManager categoryManager, IMapper mapper)
+        public ShoppingController(IProductManager productManager, ICategoryManager categoryManager, IMapper mapper, IAppUserManager appUserManager, IOrderManager orderManager, IOrderDetailManager orderDetailManager, IAddressManager addressManager)
         {
             _productManager = productManager;
             _categoryManager = categoryManager;
             _mapper = mapper;
+            _appUserManager = appUserManager;
+            _orderManager = orderManager;
+            _orderDetailManager = orderDetailManager;
+            _addressManager = addressManager;
         }
 
         [Route("/")]
@@ -107,7 +118,7 @@ namespace Project.MVCUI.Controllers
 
         public IQueryable<Product> SortProducts(IQueryable<Product> query, string? sort)
         {
-            if(sort!=null) ViewBag.selectedSort = sort;
+            if (sort != null) ViewBag.selectedSort = sort;
 
             if (sort == null) return query;
             else if (sort == "eyu") return query.OrderBy(x => x.CreatedDate);
@@ -115,6 +126,136 @@ namespace Project.MVCUI.Controllers
             else if (sort == "eyf") return query.OrderByDescending(x => x.Price);
             else if (sort == "aaz") return query.OrderBy(x => x.Name);
             else return query.OrderByDescending(x => x.Name);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ConfirmOrder()
+        {
+            string userName = User.Identity!.Name!;
+
+            var (isSuccess, error, appUser) = await _appUserManager.GetUserWithProfileAndAddressAsync(userName);
+            if (!isSuccess)
+            {
+                TempData["fail"] = error;
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (!await _addressManager.AnyAsync(x => x.AppUserProfileId == appUser!.Id))
+            {
+                TempData["fail"] = "Sipariş verebilmeniz için en az 1 adet adres girmiş olmalısınız";
+                return RedirectToAction(nameof(CartPage));
+            }
+
+            OrderWrapper wrapper = new() { AppUser = _mapper.Map<AppUserViewModel>(appUser) };
+
+            return View(wrapper);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> ConfirmOrder(OrderWrapper request)
+        {
+            ModelState.Remove("AppUser.PasswordHash");
+            ModelState.Remove("AppUser.PhoneNumber");
+            ModelState.Remove("AppUser.PasswordHashConfirm");
+            ModelState.Remove("AppUser.UserName");
+            ModelState.Remove("FullAddress");
+            if (!ModelState.IsValid) return View(request);
+
+            Cart? cart = HttpContext.Session.GetSession<Cart>("cart");
+            if (cart == null)
+            {
+                TempData["fail"] = "Sepetinizde ürün bulunmamaktadır";
+                return RedirectToAction(nameof(Index));
+            }
+
+            Order order = new()
+            {
+                TotalPrice = cart.TotalPrice,
+                AppUserId = request.AppUser!.Id,
+                AddressId = request.AddressId
+            };
+
+            var (isSuccess, error) = await _orderManager.AddAsync(order);
+            if (!isSuccess)
+            {
+                TempData["fail"] = error;
+                return RedirectToAction(nameof(Index));
+            }
+
+            List<string> orderedProductsForMailBody = new();
+
+            foreach (CartItem item in cart.Basket)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    OrderId = order.Id,
+                    ProductId = item.ID,
+                    Quantity = item.Amount,
+                    SubTotal = item.SubTotal
+                };
+
+                orderedProductsForMailBody.Add(
+                    @$"
+                    <li style='margin-bottom: 20px; padding: 20px; border: 1px solid #e0e0e0; border-radius: 10px;'>
+                    <strong>Ürün:</strong> {item.Name}
+                    <br>
+                    <strong>Fiyat:</strong> {item.Price.ToString("C2")}
+                    <br>
+                    <strong>Miktar:</strong> {item.Amount}
+                    </li>
+                    ");
+
+                var (odSuccess, odError) = await _orderDetailManager.AddAsync(orderDetail);
+                if (!odSuccess)
+                {
+                    TempData["fail"] = odError;
+                    return RedirectToAction(nameof(Index));
+                }
+
+                Product? product = await _productManager.FindAsync(item.ID);
+
+                product!.Stock -= item.Amount;
+                var (pSuccess, pError) = await _productManager.UpdateAsync(product);
+                if (!pSuccess)
+                {
+                    TempData["fail"] = pError;
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            string organizedProductsForMailBody = string.Join(null, orderedProductsForMailBody);
+            string fullAddress = (await _addressManager.Where(x => x.Id == request.AddressId && x.Status != DataStatus.Deleted).Select(x => x.FullAddress).FirstOrDefaultAsync())!;
+
+            string mailBody =
+                @$"
+      <div style='max-width: 800px; margin-top: 1%; padding: 20px; background-color: white; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); border-radius: 10px;'>
+    <div style='background-color: #2c7be5; color: white; text-align: center; padding: 15px; border-radius: 10px 10px 10px 10px; font-family: sans-serif;'>
+        <h1>TechByGamers</h1>
+    </div>
+    <div style='margin-top: 20px; padding: 20px; border: 1px solid #e0e0e0; border-radius: 10px;'>
+      <div style='margin-bottom: 20px; padding: 20px; border: 1px solid #e0e0e0; border-radius: 10px;'>
+        <h2 style='text-align: center;'>Fatura Bilgileri</h2>
+        <p><strong>Müşteri Adı:</strong> {request.AppUser.AppUserProfile!.FullName}</p>
+        <p><strong>Adres:</strong> {fullAddress}</p>
+        <p><strong>Telefon:</strong> {request.AppUser.PhoneNumber}</p>
+      </div>
+
+        <h2 style='text-align: center;'>Sipariş Detayları</h2>
+        <ul style='list-style-type: none; padding: 0; margin: 0;'>
+            {organizedProductsForMailBody}
+        </ul>
+        <p style='margin-top: 20px; text-align: right; font-size: 20px;'><strong>Toplam Tutar:</strong> {order.TotalPrice.ToString("C2")}</p>
+    </div>
+</div>
+    ";
+
+            HttpContext.Session.Remove("cart");
+            MailService.SendMailAsync(request.AppUser.Email, mailBody, "Siparişiniz için teşekkürler | TechByGamers");
+            TempData["success"] = "Siparişiniz başarıyla alınmıştır, faturanız mail adresinize gönderildi";
+
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpGet("{id}")]
---
+++ Project.MVCUI/ViewModels/WrapperClasses/OrderWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Project.MVCUI.ViewModels.WrapperClasses
+{
+    public class OrderWrapper
+    {
+        public AppUserViewModel? AppUser { get; set; }
+
+
+        //--------------------Address Information-----------------
+        [Display(Name ="Adres")]
+        [Required(ErrorMessage ="{0} seçi")]
+        public int AddressId { get; set; }
+        public string FullAddress { get; set; } = null!;
+        //--------------------------------------------------------
+
+
+        //---------------------Card Informations-------------------
+
+
+        [Display(Name = "Kart Sahibinin Adı Soyadı")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [DataType(DataType.Text)]
+        public string CardUserName { get; set; } = null!;
+
+        [Display(Name = "Güvenlik Numarası")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "Geçersiz format (222 vb.)")]
+        public string SecurityNumber { get; set; } = null!;
+
+        [Display(Name = "Kart Numarası")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [StringLength(19, MinimumLength = 19, ErrorMessage = "19 karakter olmalıdır")]
+        [RegularExpression(@"^\d{4} \d{4} \d{4} \d{4}$", ErrorMessage = "Bu alan sadece rakamlardan oluşmalı ve 16 haneli olmalıdır")]
+        public string CardNumber { get; set; } = null!;
+
+        [Display(Name = "Son kullanma Ayı")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "2 karakter olmalıdır")]
+        [RegularExpression(@"^\d{2}$", ErrorMessage = "Bu alan sadece rakamlardan oluşmalıdır")]
+        public string CardExpiryMounth { get; set; } = null!;
+
+        [Display(Name = "Son kullanma Yılı")]
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "4 karakter olmalıdır")]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Bu alan sadece rakamlardan oluşmalıdır")]
+        public string CardExpiryYear { get; set; } = null!;
+        public decimal ShoppingPrice { get; set; }
+        //----------------------------------------------------------
+    }
+}
